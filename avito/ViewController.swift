38b6c62e104fb08f94faae76c2a8058b3607c665
Talgat <lukmanovtalgat@mail.ru>
2023-08-24 20:45:34 +0300

    create project

---
+++ avito/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  avito
+//
+//  Created by Талгат Лукманов on 24.08.2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
